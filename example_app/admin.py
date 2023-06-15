79bec9d277afcd0f99c6369172d53cd878f1a214
Eric Sheen <eric.sprog@gmail.com>
2023-06-15 19:13:11 -0400

    remove useless imports in admin.py

--- example_app/admin.py
+++ example_app/admin.py
@@ -1,7 +1,5 @@
 from django_descope.models import DescopeUser
-from django.shortcuts import render
 from django.contrib import admin
-from django.urls import path
 
 
 class MyAdminSite(admin.AdminSite):
