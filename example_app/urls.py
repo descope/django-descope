0dc6b53820bbd34e6c8cb2102ae04861a84b5021
Eric Sheen <eric.sprog@gmail.com>
2023-06-15 17:08:54 -0400

    styles + reroute to admin

--- example_app/urls.py
+++ example_app/urls.py
@@ -3,10 +3,12 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 from .views import Debug
 from .admin import descope_admin_site
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="descope_login.html"), name="index"),
-    path('admin/', descope_admin_site.urls),
+    path('admin/', descope_admin_site.urls, name="admin"),
     path("debug", Debug.as_view(), name="debug"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
