edd29e72bba2f8ebac0870021edcc20f581823da
Omer Cohen <omer@descope.com>
2023-06-25 22:14:46 -0700

    built in default admin login template

--- example_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-descope_admin_site = admin.site
-
-# override the login template with our own
-descope_admin_site.login_template = "admin_login.html"
--- example_app/urls.py
+++ example_app/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .admin import descope_admin_site
 from .views import Debug
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="descope_login.html"), name="index"),
-    path("admin/", descope_admin_site.urls, name="admin"),
     path("debug", Debug.as_view(), name="debug"),
 ]
