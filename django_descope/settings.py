8af85a2e1f4294cbe94080a0f1b4784d3c72e0cc
Omer Cohen <omer@descope.com>
2024-08-18 17:04:55 +0300

    feat: pin descope/web-component (#199)

--- django_descope/settings.py
+++ django_descope/settings.py
@@ -1,8 +1,14 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
+# renovate depName=@descope/web-component datasource=npm
+WEB_COMPONENT_VERSION = getattr(settings, "DESCOPE_WEB_COMPONENT_VERSION", "3.23.1")
 WEB_COMPONENT_SRC = getattr(
-    settings, "DESCOPE_WEB_COMPONENT_SRC", "https://unpkg.com/@descope/web-component"
+    settings,
+    "DESCOPE_WEB_COMPONENT_SRC",
+    "https://unpkg.com/@descope/web-component@{:s}/dist/index.js".format(
+        WEB_COMPONENT_VERSION
+    ),
 )
 
 MANAGEMENT_KEY = getattr(settings, "DESCOPE_MANAGEMENT_KEY", "")
